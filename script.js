6609e17f351674ac8848db79de61b341155e5a1c
thomas nguyen <thomasnguyen@MacBook-Pro-von-thomas.local>
2022-09-17 11:37:51 +0200

    Set slideshow for whole home section + set fixed navigation bar + WIP: Work on viewport response, so code recognizes which section is currently active

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let slideIndex = 0;
+var slideshowTimeout;
 showSlides();
 
-function showSlides() {
+function currentSlide(n) {
+  clearTimeout(slideshowTimeout);
+  showSlides((slideIndex = n - 1));
+}
+
+function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("home-image-container");
-  //   let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("home-container");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -12,10 +18,39 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  //   for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  //   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
-  //   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  dots[slideIndex - 1].className += " active";
+  slideshowTimeout = setTimeout(showSlides, 700000); // Change image every 10 seconds
+}
+
+var homeSection = document.getElementById("home-section");
+var aboutSection = document.getElementById("about-section");
+var myElement = document.querySelector(".test-viewport");
+
+// window.onscroll = function () {
+//   elementInViewport();
+// };
+
+function elementInViewport() {
+  var bounding = myElement.getBoundingClientRect();
+  var myElementHeight = myElement.offsetHeight;
+  var myElementWidth = myElement.offsetWidth;
+
+  if (
+    bounding.top >= -myElementHeight &&
+    bounding.left >= -myElementWidth &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth) +
+        myElementWidth &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) +
+        myElementHeight
+  ) {
+    alert("Element is in the viewport!");
+  } else {
+    alert("Element is NOT in the viewport!");
+  }
 }
